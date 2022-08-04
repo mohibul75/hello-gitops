83ade4a66d0be859efd230db8913187f3a05fc5a
Mohibul Alam <bsse1004@iit.du.ac.bd>
2022-08-04 11:57:36 +0600

    terroform file added

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = var.dnspreffix
-default_node_pool {
+node_pool {
     name       = "default"
     node_count = var.agentnode
     vm_size    = var.size
